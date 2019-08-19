5d6338471f2cf4b8c8bf5bde2b829af267fc197c
FunnyHopping <dsajikgtre00@yahoo.co.jp>
2019-08-19 22:17:42 +0900

    define_action_index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.where(params[:name])
+    respond_to do |format|
+      format.json
+    end
   end
 
   def edit
---
+++ app/views/users/index.json.jbuilder
@@ -0,0 +1,3 @@
+json.array! @users do |user|
+  json.name  @tweet.name
+end

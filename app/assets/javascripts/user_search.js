b021566d6de96cf327b368af5f64e0e1d2c7deab
FunnyHopping <dsajikgtre00@yahoo.co.jp>
2019-08-21 16:41:57 +0900

    script_incremental

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,19 +1,22 @@
 $(function() {
-  function buildHtml(user) {
-    var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">${user.name}</p>
-                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
-                </div>`
-    return html;
-  }
-  function appendList(item) {
-    $('#user-search-result').append(item);
-  }
 
   $('#user-search-field').on('keyup', function(e) {
+  
+    function buildListHtml(user) {
+      var listHtml = `<div class="chat-group-user clearfix">
+                    <p class="chat-group-user__name">${user.name}</p>
+                    <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                    </div>`
+      return listHtml;
+    }
+    function appendList(item) {
+      $('#user-search-result').append(item);
+    }
+  
     e.preventDefault();
     var list
     var input = $(this).val();
+  
     $.ajax({
       url: '/users',
       type: 'GET',
@@ -22,21 +25,45 @@ $(function() {
       processData: false,
       contentType: false
     })
+  
     .done(function(users) {
       list = users
       if (prelist !== list)
         $('#user-search-result').html('');
-        $.each(users, function(index, user){
-          var html = buildHtml(user);
-          appendList(html);
+        $.each(list, function(index, user){
+          var listHtml = buildListHtml(user);
+          appendList(listHtml);
         });
       if (users.length === 0) {
         appendList('<div class="chat-group-user clearfix"><p class="chat-group-user__name">一致するユーザーがいません。</p>')
       }
     })
+  
     .fail(function() {
       alert('検索に失敗しました。')
     });
     prelist = list
   });
+
+  $(document).on('click', '.user-search-add', function() {
+    function buildMemberHtml(user_id, user_name) {
+      var memberHtml = `<div class='chat-group-user clearfix js-chat-member' id="${user_id}">
+                  <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+                  <p class='chat-group-user__name'>${user_name}</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                  </div>`
+      return memberHtml;
+    }
+
+    var userId = $(this).attr('data-user-id')
+    var userName = $(this).attr('data-user-name')
+
+    var memberHtml = buildMemberHtml(userId, userName)
+    $('.js-add-user').append(memberHtml);
+    $(this).parent().remove();
+  });
+
+  $(document).on('click', '.js-remove-btn', function() {
+    $(this).parent().remove();
+  })
 });

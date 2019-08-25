3a20b313be35f314ff099c32afca40f305d86a4b
FunnyHopping <dsajikgtre00@yahoo.co.jp>
2019-08-25 19:11:13 +0900

    S3

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,6 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
@@ -53,7 +52,6 @@ set :linked_files, %w{ config/secrets.yml }
 
 # 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
 
-after 'deploy:publishing', 'deploy:restart'
 
 
 
